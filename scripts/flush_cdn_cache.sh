74841430104dca7dc746e03a551837218195607e
Anton Samarchyan <desecho@gmail.com>
2022-12-05 00:12:04 -0500

    Changes

--- scripts/flush_cdn_cache.sh
+++ scripts/flush_cdn_cache.sh
@@ -4,8 +4,7 @@ IFS=$'\n\t'
 
 CDN_ID="$(
   doctl compute cdn list -o json \
-    | jq --arg SPACE_NAME "${SPACE_NAME}" '.[] \
-    | select(.custom_domain==$SPACE_NAME).id' -r
+    | jq --arg SPACE_NAME "${SPACE_NAME}" '.[] | select(.custom_domain==$SPACE_NAME).id' -r
 )"
 
 doctl compute cdn flush "${CDN_ID}"
